452569f2a70d16222b1539d5a4bea35e9fc9c312
Denis_Bredun <bredun.denis@gmail.com>
2025-07-20 02:21:19 +0300

    Removed class Math (+1 squashed commits)

--- Program.cs
+++ Program.cs
@@ -9,10 +9,4 @@ namespace MergeRebaseSquashTest
             Console.WriteLine("Hi world");
         }
     }
-
-    public static class Math
-    {
-        public static double Sum(double x1, double x2) => x1 + x2;
-        public static double Subtract(double x1, double x2) => x1 - x2;
-    }
 }
