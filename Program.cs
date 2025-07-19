90c0a9e04a2a3186ef3aecd11a125540e0a3b771
Denis_Bredun <bredun.denis@gmail.com>
2025-07-20 01:42:22 +0300

    Method Sum

--- Program.cs
+++ Program.cs
@@ -9,4 +9,12 @@ namespace MergeRebaseSquashTest
             Console.WriteLine("Hello, World!");
         }
     }
+
+    public static class Math
+    {
+        public static double Sum(double x1, double x2)
+        {
+            return x1 + x2;
+        }
+    }
 }
