d7ac28689077dff681c9a47a2f7d58a38732a71a
Denis_Bredun <bredun.denis@gmail.com>
2025-07-20 02:12:34 +0300

    Reset

--- Program.cs
+++ Program.cs
@@ -6,15 +6,7 @@ namespace MergeRebaseSquashTest
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Hi world");
         }
     }
-
-    public static class Math
-    {
-        public static double Sum(double x1, double x2) => x1 + x2;
-        public static double Subtract(double x1, double x2) => x1 - x2;
-    }
-
-    public record Person(string Name, int Age);
 }
